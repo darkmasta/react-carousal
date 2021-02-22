aa1f9c2a576018f115adc4de2fca31bd55705a73
darkmasta <alperen_bagci@hotmail.com>
2021-02-22 22:51:01 +0300

    First commit

---
+++ src/sliderData.js
@@ -0,0 +1,24 @@
+var sliderData = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1612626256147-08018dd22040?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1612589825438-05c0570152d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1612599538302-de0c763349b3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1612589569109-cb477f02b00c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1612543827431-f1f1eafb767c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80",
+  },
+];
+
+export default sliderData;
